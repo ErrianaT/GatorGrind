dcac197fe25cb9b7fec48e65c99ff0c7ea8169c8
ErrianaT <thomaserriana@ufl.edu>
2026-04-15 13:10:56 -0400

    separated business route from auth route

---
+++ backend/routes/businessRoutes.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const Business = require("./models/Business");
+
+const router = express.Router();
+
+// creating a business route 
+router.post("/create", async (req, res) => {
+  try {
+    const {
+      businessName,
+      address1,
+      address2,
+      city,
+      state,
+      zipCode,
+      webAddress,
+      description,
+      category,
+      owner
+    } = req.body;
+
+    const newBusiness = new Business({
+      business_name: businessName,
+      owner: owner,
+      address: {
+        address1,
+        address2,
+        city,
+        state,
+        zipCode
+      },
+      website_url: webAddress,
+      description,
+      category,
+      rating: 0,
+      is_verified: false
+    });
+
+    await newBusiness.save();
+
+    res.status(201).json({
+      message: "Business created successfully",
+      business: newBusiness
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating business" });
+  }
+});
